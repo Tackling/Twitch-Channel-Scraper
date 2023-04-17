f82a35d6d0c75fd2c54fd868d8d1f36521770c94
Rv <129611072+daddyRv@users.noreply.github.com>
2023-04-17 16:42:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const CLIENT_ID = 'CLIENT-ID-HERE';
-const AUTH_TOKEN = 'ACCESS-TOKEN-HERE';
-const CATEGORIES = ['Just Chatting'];
-const MIN_VIEWERS = 100;
+const CLIENT_ID = 'CLIENT-ID-HERE'; // Replace 'CLIENT-ID-HERE' with your actual Twitch API client ID
+const AUTH_TOKEN = 'ACCESS-TOKEN-HERE'; // Replace 'ACCESS-TOKEN-HERE' with your actual Twitch API access token
+const CATEGORIES = ['Just Chatting']; // Add more categories by including additional string elements separated by commas
+const MIN_VIEWERS = null; // Set a minimum number of viewers to filter streams, or leave as null to disable filtering
 
 async function fetchGameId(category) {
   try {
@@ -38,7 +38,7 @@ async function fetchChannels(gameId, category) {
       channels.push(...response.data.data);
       cursor = response.data.pagination.cursor;
       processChannels(response.data.data, category);
-      await new Promise(resolve => setTimeout(resolve, 5000)); // wait for 5 seconds before making the next request
+      await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 seconds before making the next request
     } while (true);
   } catch (error) {
     console.error(error);
@@ -47,7 +47,8 @@ async function fetchChannels(gameId, category) {
 
 function processChannels(channels, category) {
   const channelsDirectory = './channels';
-  const filePath = path.join(channelsDirectory, `${category}.txt`);
+  const categoryFilePath = path.join(channelsDirectory, `${category.replace(':', '_')}.txt`);
+  const allFilePath = path.join(channelsDirectory, 'All.txt');
   const existingChannels = new Set();
 
   // Create the channels directory if it doesn't exist
@@ -55,10 +56,10 @@ function processChannels(channels, category) {
     fs.mkdirSync(channelsDirectory);
   }
 
-  if (fs.existsSync(filePath)) {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const lines = data.split(/\r?\n/);
-    lines.forEach(line => {
+  if (fs.existsSync(allFilePath)) {
+    const allData = fs.readFileSync(allFilePath, 'utf8');
+    const allLines = allData.split(/\r?\n/);
+    allLines.forEach(line => {
       if (line.trim() !== '') {
         existingChannels.add(line.trim());
       }
@@ -69,9 +70,13 @@ function processChannels(channels, category) {
   uniqueChannels.forEach(channel => {
     const username = channel.user_login;
     console.log(`Found channel: ${username}`);
-    fs.appendFile(filePath, `${username}\n`, err => {
+    fs.appendFile(categoryFilePath, `${username}\n`, err => {
       if (err) console.error(err);
-      else console.log(`Saved username: ${username}`);
+      else console.log(`Saved username: ${username} to ${category}.txt`);
+    });
+    fs.appendFile(allFilePath, `${username}\n`, err => {
+      if (err) console.error(err);
+      else console.log(`Saved username: ${username} to All.txt`);
     });
   });
 }
